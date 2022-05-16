3057999aef307205e8469d9f7f4dedb314f9e3a0
Chaewon Song <81732426+chaewonS@users.noreply.github.com>
2022-05-16 21:54:25 +0900

    [채원] ajax 설문조사 결과 json 출력

--- back-end/one-week/main.py
+++ back-end/one-week/main.py
@@ -3,6 +3,8 @@ from flask_pymongo import PyMongo
 from pymongo import MongoClient
 import datetime
 from flask import flash
+from bson.json_util import dumps
+from sqlalchemy import JSON
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "2019"
@@ -12,13 +14,58 @@ mongo = PyMongo(app)
 # Mongo DB
 client = MongoClient('localhost', 27017)
 members = mongo.db.members
+result = mongo.db.survey_result
 
 @app.route('/')
 def home():
     return render_template('one_week.html')
+    # return render_template('yaho.html')
     # return render_template('test.html')
     # return render_template('survey.html') #main html
     # return render_template('research.html')
+
+@app.route('/ajax', methods=['GET', 'POST'])
+def ajax():
+    data = request.get_json()
+    # print(data)
+    keyList = data.keys()
+    # print(data.keys())
+    # print(data.items())
+    # print(data.values())
+    #생활습관 설문조사 결과 {'name':~, 'value':} 형태
+    q1 = list(data.values())[0][0:2]
+    print('q1',q1)
+    q2 = list(data.values())[0][2]
+    print('q2',q2)
+    q3 = list(data.values())[0][3]
+    print('q3',q3)
+    q4 = list(data.values())[0][4]
+    print('q4',q4)
+    q5 = list(data.values())[0][5]
+    print('q5',q5)
+    q6 = list(data.values())[0][6:15]
+    print('q6',q6)
+    q7 = list(data.values())[0][16]
+    print('q7',q7)
+    q8 = list(data.values())[0][17]
+    print('q8',q8)
+    q9 = list(data.values())[0][18]
+    print('q9',q9)
+    #정신건강 설문조사 결과 {'name':~, 'value':} 형태
+    mq1 = list(data.values())[1][0]
+    print('mental-q1',mq1)
+    mq2 = list(data.values())[1][1]
+    print('mental-q2',mq2)
+    mq3 = list(data.values())[1][2]
+    print('mental-q3',mq3)
+    mq4 = list(data.values())[1][3]
+    print('mental-q4',mq4)
+    # for item in keyList:
+    #     print('Key:',item, 'Value:',data[item])
+    # members.insert_one(data)
+    #request.get_json()를 통해 받아 data에 넣기
+    members.insert_one(q2)
+    return jsonify(result = "success", result2= data)
 
 #회원가입
 @app.route('/register', methods=['GET','POST'])
